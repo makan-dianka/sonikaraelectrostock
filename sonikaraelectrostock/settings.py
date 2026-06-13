18c523ae8df115b3c0eab6da0e2f60cdcbee9443
Makan DIANKA <makan.dianka@hotmail.com>
2026-06-14 00:37:18 +0200

    install stores app

--- sonikaraelectrostock/settings.py
+++ sonikaraelectrostock/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'accounts',
     'dashboard',
+    'stores',
 ]
 
 MIDDLEWARE = [
@@ -90,6 +91,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
