161f8976c377dc952a61fb0bfb9b8a30e53cf37a
Makan DIANKA <makan.dianka@hotmail.com>
2026-06-14 16:03:20 +0200

    user will no longer be able to create, modify, delete a store without being connected without being a store admin

--- stores/views.py
+++ stores/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+
 from .models import Store
 from .forms import StoreForm
 
 
 # Liste de magasins
+@login_required(login_url='accounts:login')
 def store_list(request):
     stores = Store.objects.filter(deleted=False).order_by('-created_at')
     return render(request, 'stores/store_list.html', {'stores': stores})
@@ -11,7 +15,11 @@ def store_list(request):
 
 
 # creation d'un magasin
+@login_required(login_url='accounts:login')
 def create_store(request):
+    if request.user.role not in ['owner']:
+        return HttpResponseForbidden("Vous n'avez pas la permission à créer un Magasin.")
+    
     form = StoreForm(request.POST or None)
     if form.is_valid():
         form.save()
@@ -21,7 +29,11 @@ def create_store(request):
 
 
 # mise à jour d'un magasin
+@login_required(login_url='accounts:login')
 def update_store(request, pk):
+    if request.user.role not in ['owner']:
+        return HttpResponseForbidden("Vous n'avez pas la permission à mettre à jour un Magasin.")
+    
     store = get_object_or_404(Store, id=pk)
     form = StoreForm(request.POST or None, instance=store)
     if form.is_valid():
@@ -30,8 +42,10 @@ def update_store(request, pk):
     return render(request, 'stores/store_form.html', {'form':form})
 
 
-
+@login_required(login_url='accounts:login')
 def delete_store(request, pk):
+    if request.user.role not in ['owner']:
+        return HttpResponseForbidden("Vous n'avez pas la permission de supprimer un Magasin.")
     store = get_object_or_404(Store, id=pk)
     store.deleted = True
     store.save()
