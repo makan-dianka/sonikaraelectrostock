96f1342a30086202e308bfad7cda0690d991a07c
Makan DIANKA <makan.dianka@hotmail.com>
2026-06-16 15:37:00 +0200

    automatically generate the invoice number

--- purchases/models.py
+++ purchases/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from sonikaraelectrostock.models import TimeStampedModel
+from django.utils import timezone
 
 
 class Purchase(TimeStampedModel):
@@ -25,7 +26,8 @@ class Purchase(TimeStampedModel):
     invoice_number = models.CharField(
         max_length=100,
         blank=True,
-        null=True
+        null=True,
+        unique=True
     )
 
     purchase_date = models.DateField()
@@ -48,7 +50,70 @@ class Purchase(TimeStampedModel):
 
     def __str__(self):
         return f"Achat #{self.id}"
-    
+
+    def update_total(self):
+        total = sum(item.quantity * item.unit_price for item in self.items.all())
+        self.total_amount = total
+        self.save()
+
+
+    def can_change_to(self, new_status):
+
+        transitions = {
+            'draft': [
+                'received',
+                'cancelled'
+            ],
+
+            'received': [],
+
+            'cancelled': []
+        }
+
+        return (new_status in transitions[self.status])
+
+
+
+
+
+    def generate_invoice_number(self):
+        """genere invoice number
+
+        Returns:
+            str: invoice number
+        """
+
+        today = timezone.now()
+
+
+        prefix = (f"FACT-ACH-" f"{today:%Y%m}-")
+
+        last = (
+            Purchase.objects.filter(
+                invoice_number__startswith=prefix
+            ).order_by(
+                '-invoice_number'
+            ).first()
+        )
+
+        if last:
+
+            try:
+                seq = int(last.invoice_number.split('-')[-1]) + 1
+            except:
+                seq = 1
+        else:
+            seq = 1
+        return (f"{prefix}" f"{seq:04d}")
+
+
+
+    def save(self, *args, **kwargs):
+
+        if not self.invoice_number:
+            self.invoice_number = self.generate_invoice_number()
+        super().save(*args, **kwargs)
+
 
 
 
