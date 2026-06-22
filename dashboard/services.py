70d7f19dd1f665fdf77f6483be54b073861e82e4
Makan DIANKA <makan.dianka@hotmail.com>
2026-06-22 13:08:16 +0200

    refactory

--- dashboard/services.py
+++ dashboard/services.py
@@ -1,37 +1,6 @@
 def percentage_change(current, previous):
-
     if previous == 0:
-
         if current > 0:
-
             return 100
-
         return 0
-
-    return round(
-
-        (
-
-            (
-
-                current
-
-                -
-
-                previous
-
-            )
-
-            /
-
-            previous
-
-        )
-
-        *
-
-        100,
-
-        2
-
-    )
+    return round(((current - previous) / previous) * 100, 2)
