949bc2fd6db953ece4730138708217aff1a6c852
Makan DIANKA <makan.dianka@hotmail.com>
2026-06-22 15:13:23 +0200

    admin panel adte

--- sales/admin.py
+++ sales/admin.py
@@ -4,4 +4,4 @@ from .models import Sale
 
 @admin.register(Sale)
 class SaleAdmin(admin.ModelAdmin):
-    list_display = ('status', 'store', 'total', 'payment_status', 'user')
+    list_display = ('status', 'store', 'total', 'payment_status', 'user', 'created_at')
