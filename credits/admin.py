01cef024cfa367d773254307e6aaa23450793e76
Makan DIANKA <makan.dianka@hotmail.com>
2026-07-06 03:05:39 +0200

    ajout credit dans admin panel

--- credits/admin.py
+++ credits/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Credit, CreditPayment
 
-# Register your models here.
+
+@admin.register(Credit)
+class CreditAdmin(admin.ModelAdmin):
+    list_display = ('reference', 'customer', 'store', 'amount', 'due_date', 'status', 'created_at')
+
+
+
+@admin.register(CreditPayment)
+class CreditPaymentAdmin(admin.ModelAdmin):
+    list_display = ('reference', 'credit', 'payment_method', 'amount', 'note', 'created_at')
+
