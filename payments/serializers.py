f6c67c7285aac5132c04791fda87bbb06a52a9e0
Makan DIANKA <makan.dianka@hotmail.com>
2026-07-12 11:28:59 +0200

    fix bouton

--- payments/serializers.py
+++ payments/serializers.py
@@ -8,11 +8,19 @@ class PaymentSearchSerializer(serializers.ModelSerializer):
     created_at = serializers.SerializerMethodField()
     sale = serializers.CharField(source='sale.reference', read_only=True)
     purchase = serializers.CharField(source='purchase.reference', read_only=True)
+    sale_id = serializers.SerializerMethodField()
+    purchase_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Payment
-        fields = ['id', 'sale', 'purchase', 'amount', 'get_remaining', 'payment_method', 'reference', 'created_at']
+        fields = ['id', 'sale', 'sale_id', 'purchase', 'purchase_id', 'amount', 'get_remaining', 'payment_method', 'reference', 'created_at']
 
 
     def get_created_at(self, obj):
         return date_format(obj.created_at, "j F Y")
+    
+    def get_sale_id(self, obj):
+        return obj.sale.id if obj.sale else None
+
+    def get_purchase_id(self, obj):
+        return obj.purchase.id if obj.purchase else None
