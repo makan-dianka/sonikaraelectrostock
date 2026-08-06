d7c7713da9687937acd1870692b223e8d6a75d14
Makan DIANKA <makan.dianka@hotmail.com>
2026-08-06 13:33:26 +0200

    ajout de comapny manager pour filtrer

--- sonikaraelectrostock/models.py
+++ sonikaraelectrostock/models.py
@@ -9,6 +9,12 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+
+class CompanyManager(models.Manager):
+    def for_company(self, company):
+        return self.filter(company=company)
+
+
 class CompanyOwnedModel(models.Model):
     company = models.ForeignKey(
         "subscriptions.Company",
@@ -17,6 +23,8 @@ class CompanyOwnedModel(models.Model):
         on_delete=models.CASCADE,
         related_name="%(class)ss"
     )
+
+    objects = CompanyManager()
 
     class Meta:
         abstract = True
